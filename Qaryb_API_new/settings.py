421761e6c0eaa167af66a94fb5c0b1cdd39899fc
Youness <youness@iMac-de-Youness.local>
2022-02-24 11:22:37 +0100

    test: -next 2

--- Qaryb_API_new/settings.py
+++ Qaryb_API_new/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django.contrib.postgres',
     # 'channels',
     'rest_framework',
