9a7074f9c29de0529fea3598c2788a6ae563ece6
Youness <youness@iMac-de-Youness.local>
2022-03-14 16:04:09 +0100

    fix: daphne apps not loaded yet

--- Qaryb_API_new/asgi.py
+++ Qaryb_API_new/asgi.py
@@ -18,9 +18,11 @@
 # #     ),
 # # })
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
+django.setup()
 from django.core.asgi import get_asgi_application
 from chat.jwt_middleware import SimpleJwtTokenAuthMiddleware
+from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qaryb_API_new.settings")
