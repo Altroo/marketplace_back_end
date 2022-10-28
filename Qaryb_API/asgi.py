9048ff531ff963504dcc7c5ce7e767bfcadcaae2
Youness imac <y.elalami@qaryb.com>
2022-10-28 10:30:44 +0100

    fix : asgi.py

--- Qaryb_API/asgi.py
+++ Qaryb_API/asgi.py
@@ -1,13 +1,13 @@
 import os
-# # Avoid daphne apps not loaded yet
+# Avoid daphne apps not loaded yet
 import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qaryb_API.settings")
 django.setup()
 from django.core.asgi import get_asgi_application
 from chat.jwt_middleware import SimpleJwtTokenAuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qaryb_api.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -17,14 +17,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Qaryb_API.settings')
-#
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": SimpleJwtTokenAuthMiddleware(
-#         URLRouter(
-#             websocket_urlpatterns
-#         )
-#     ),
-# })
