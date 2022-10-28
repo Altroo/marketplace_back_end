fde6ffb874f54dae1a57c8327cd37a5680596815
Youness imac <y.elalami@qaryb.com>
2022-10-28 10:23:07 +0100

    fix: subscription migrations to gitignore, fix asgi.py settings

--- Qaryb_API/asgi.py
+++ Qaryb_API/asgi.py
@@ -1,13 +1,13 @@
 import os
 # # Avoid daphne apps not loaded yet
 import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qaryb_API.settings")
 django.setup()
 from django.core.asgi import get_asgi_application
 from chat.jwt_middleware import SimpleJwtTokenAuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qaryb_API.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
