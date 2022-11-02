f42b0c2ded599b4195b950b4242eeeccef3c01b1
Yooy <scorpion.yooy@gmail.com>
2022-11-02 09:47:56 +0100

    fix: celery configs

--- Qaryb_API/celery_conf.py
+++ Qaryb_API/celery_conf.py
@@ -8,6 +8,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qaryb_API.settings")
 app = Celery('Qaryb_API', broker=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.timezone = settings.TIME_ZONE
+app.conf.setdefault('worker_cancel_long_running_tasks_on_connection_loss', True)
 app.autodiscover_tasks(
     packages=(
         'account.base.tasks',
